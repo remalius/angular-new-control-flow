19356065b7badf52f1a4433d4c840003acee7bd4
Janusz Kina <janusz@gmail.com>
2023-11-05 22:30:40 +0100

    Code reformat

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import {Routes} from '@angular/router';
-import {DogsComponent} from "./dogs/dogs.component";
+import { Routes } from '@angular/router';
+import { DogsComponent } from "./dogs/dogs.component";
 
 export const routes: Routes = [{path: '', component: DogsComponent}];
